51bd295527c00ad571593eb27ea54444d648913f
Joe Cowton <joecowton@gmail.com>
2017-12-31 12:14:06 +0000

    tests update qulaity function

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -16,7 +16,35 @@ describe("Gilded Rose", function() {
     });
   });
 
-  
+  describe("Items", function() {
+    beforeEach(function (){
+      item = { name: 'foo', sellIn: 2, quality: 4};
+      gildedRose = new Shop([item]);
+    });
+
+    it("shop contains item", function() {
+      expect(gildedRose.items[0]).toEqual({ name: 'foo', sellIn: 2, quality: 4})
+    });
+
+    it("update quality should decrease quality by 1", function(){
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].quality).toEqual(3);
+    });
+
+    it("update quality should reduce sellIn value by 1", function() {
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].sellIn).toEqual(1);
+    });
+
+    it('Quality of an item decreases twice as fast once it has expired', function() {
+      item = { name: 'foo', sellIn: -1, quality: 4 }
+      gildedRose = new Shop([ item ]);
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].quality).toEqual(2)
+    });
+  });
+
+
 
 
 
