4808184df3f541d328dfa8cfdf5d5e475288773d
Joe Cowton <joecowton@gmail.com>
2017-12-31 12:47:51 +0000

    tests for backstage passes

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -23,7 +23,7 @@ describe("Gilded Rose", function() {
     });
 
     it("shop contains item", function() {
-      expect(gildedRose.items[0]).toEqual({ name: 'foo', sellIn: 2, quality: 4})
+      expect(gildedRose.items[0]).toEqual({ name: 'foo', sellIn: 2, quality: 4 });
     });
 
     it("update quality should decrease quality by 1", function() {
@@ -37,34 +37,61 @@ describe("Gilded Rose", function() {
     });
 
     it('Quality of an item decreases twice as fast once it has expired', function() {
-      item = { name: 'foo', sellIn: -1, quality: 4 }
+      item = { name: 'foo', sellIn: -1, quality: 4 };
       gildedRose = new Shop([ item ]);
       gildedRose.updateQuality();
-      expect(gildedRose.items[0].quality).toEqual(2)
+      expect(gildedRose.items[0].quality).toEqual(2);
     });
   });
 
   describe('Sulfuras', function() {
     beforeEach(function (){
-      item = { name: 'Sulfuras, Hand of Ragnaros', sellIn: 0, quality: 80 }
-      gildedRose = new Shop( [ item ] )
+      item = { name: 'Sulfuras, Hand of Ragnaros', sellIn: 0, quality: 80 };
+      gildedRose = new Shop( [ item ] );
     });
 
     it('Item quality should not change', function (){
-      gildedRose.updateQuality()
-      expect(gildedRose.items[0].quality).toEqual(80)
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].quality).toEqual(80);
     });
 
     it('Item sellIn should not change', function (){
-      gildedRose.updateQuality()
-      expect(gildedRose.items[0].sellIn).toEqual(0)
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].sellIn).toEqual(0);
     });
-
   });
 
+  describe('Aged Brie', function() {
+    beforeEach(function (){
+      item = { name: 'Aged Brie', sellIn: 5, quality: 5 };
+      gildedRose = new Shop([ item ]);
+    });
 
+    it('should increase in quality on update', function(){
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].quality).toEqual(6);
+    });
 
+    it('sellin value should reduce on update', function(){
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].sellIn).toEqual(4);
+    });
+  });
 
+  describe('Backstage passes', function() {
+    beforeEach(function (){
+      item = { name:'Backstage passes to a TAFKAL80ETC concert', sellIn: 11, quality: 14 };
+      gildedRose = new Shop([ item ]);
+    });
 
+    it('should increase in quality on update', function() {
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].quality).toEqual(15);
+    });
 
+    it('should increase in quality on update', function() {
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].quality).toEqual(15);
+    });
+  });
 });
